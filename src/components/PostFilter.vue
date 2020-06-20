e57f9a9ed3c4ef94857b45f17f0c836cbed1222f
Matt <mgould@breakingfreegroup.com>
2020-06-20 18:07:25 +0100

    post filters

---
+++ src/components/PostFilter.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-card>
+    <div class='post-filter-container'>
+      <v-card-actions>
+        <div :key='item' v-for='item in Object.keys(filters)' :class='filter === item && "selected"' @click='selectFilter(item)'>
+          <v-icon dense>{{ filters[item] }}</v-icon>{{ item }}
+        </div>
+      </v-card-actions>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: [
+    'filter'
+  ],
+  data: function () {
+    return {
+      filters: {
+        Best: 'keyboard_arrow_up',
+        Hot: 'fireplace',
+        New: 'fiber_new',
+        Top: 'stars'
+      }
+    }
+  },
+  methods: {
+    selectFilter (filter) {
+      this.$emit('selectFilter', filter)
+    }
+  },
+  mounted () {
+    console.log(this.filter)
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .post-filter-container {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 3px 0;
+    margin: 20px 0;
+  }
+  .v-card__actions {
+    >div {
+      margin-right: 15px;
+      display: flex;
+      padding: 3px 6px 3px 3px;
+      border-radius: 10px;
+      cursor: pointer;
+    }
+    .selected {
+      background-color: #dcdcdc;
+    }
+  }
+  i {
+    margin-right: 3px;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
       <v-col cols='12' :md='8'>
         <CreatePostHeader v-if='isAuthenticated' />
         <p v-else>Popular posts</p>
+        <PostFilter :filter='filter' v-on:selectFilter='selectFilter' />
       </v-col>
       <v-col :md='4' v-if='$vuetify.breakpoint.mdAndUp'>
         <div>
@@ -17,11 +18,26 @@
 <script>
 // @ is an alias to /src
 import CreatePostHeader from '@/components/CreatePostHeader.vue'
+import PostFilter from '@/components/PostFilter.vue'
 
 export default {
   name: 'Home',
   components: {
-    CreatePostHeader
+    CreatePostHeader,
+    PostFilter
+  },
+  data: function () {
+    return {
+      filter: 'Best',
+      posts: [
+
+      ]
+    }
+  },
+  methods: {
+    selectFilter (filter) {
+      this.filter = filter
+    }
   },
   computed: {
     isAuthenticated () {
