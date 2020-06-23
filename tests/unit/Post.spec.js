5bb0e199528c17a51a65819a2e4c93b8df02130b
Matt <mgould@breakingfreegroup.com>
2020-06-23 14:07:35 +0100

    unit testing

---
+++ tests/unit/Post.spec.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+
+import Post from '@/components/Post.vue'
+
+Vue.use(Vuetify)
+
+const localVue = createLocalVue()
+localVue.use(Vuetify)
+
+describe('Post.vue', () => {
+  let vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
+  it('renders all post props when passed', () => {
+    const post = {
+      communityName: 'cats',
+      user: {
+        username: 'matt'
+      },
+      createdAt: Date.now(),
+      title: 'test title',
+      text: 'test text'
+    }
+
+    const wrapper = shallowMount(Post, {
+      localVue,
+      vuetify,
+      propsData: {
+        post
+      },
+      mocks: {
+        $vuetify: {
+          breakpoint: {}
+        }
+      }
+    })
+
+    const community = wrapper.find('.post-community')
+
+    expect(community.text()).toBe(`r/${post.communityName}`)
+  })
+})
---
+++ tests/unit/PostList.spec.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+import { mount, createLocalVue } from '@vue/test-utils'
+
+import PostList from '@/components/PostList.vue'
+import Post from '@/components/Post.vue'
+
+Vue.use(Vuetify)
+
+const localVue = createLocalVue()
+localVue.use(Vuetify)
+
+describe('PostList.vue', () => {
+  let vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
+  it('renders all posts', () => {
+    const post = {
+      communityName: 'cats',
+      user: {
+        username: 'matt'
+      },
+      createdAt: Date.now(),
+      title: 'test title',
+      text: 'test text'
+    }
+    const posts = [
+      post, post, post
+    ]
+
+    const wrapper = mount(PostList, {
+      localVue,
+      vuetify,
+      propsData: {
+        posts
+      },
+      mocks: {
+        $vuetify: {
+          breakpoint: {}
+        }
+      }
+    })
+
+    expect(wrapper.findAllComponents(Post).length).toEqual(3)
+  })
+})
