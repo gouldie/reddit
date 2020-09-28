e5e983e215d469dc5c46a3e42c0914e025015bbb
Matt <matt@Matts-MacBook-Pro.local>
2020-09-28 17:58:36 +0100

    auto lint + update packages

--- vue.config.js
+++ vue.config.js
@@ -12,9 +12,18 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+  chainWebpack: config => {
+    config.module
+      .rule('eslint')
+      .use('eslint-loader')
+      .options({
+        fix: true,
+      })
+  },
   configureWebpack: {
     performance: {
       hints: false
     }
-  }
+  },
+  lintOnSave: true
 }
