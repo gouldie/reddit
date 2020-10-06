090c0b8d8eacdfd62a89ef9b9644397645265e23
Matt <matt@Matts-MacBook-Pro.local>
2020-10-06 17:14:53 +0100

    improved PostList test

--- tests/unit/PostList.spec.js
+++ tests/unit/PostList.spec.js
@@ -1,96 +1,52 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import VueRouter from 'vue-router'
-import routes from '@/router/routes'
 
 import { mount, createLocalVue } from '@vue/test-utils'
 
+import routes from '@/router/routes'
 import PostList from '@/components/Posts/PostList.vue'
 import PostPreview from '@/components/Posts/PostPreview.vue'
+import posts from './fixtures/posts.json'
 
 const router = new VueRouter({ routes })
 
 Vue.use(Vuetify)
 
 const localVue = createLocalVue()
-localVue.use(Vuetify)
+
 localVue.use(VueRouter)
 
-describe('PostList.vue', () => {
-  let vuetify
-
-  beforeEach(() => {
-    vuetify = new Vuetify()
-  })
-
-  it('renders all posts', () => {
-    const post = {
-      communityName: 'cats',
-      communityId: '1',
-      user: {
-        username: 'matt'
-      },
-      createdAt: Date.now(),
-      title: 'test title',
-      text: 'test text'
-    }
-    const posts = [
-      post, post, post
+const factory = (values = {}) => {
+  return mount(PostList, {
+    localVue,
+    router,
+    propsData: {
+      posts
+    },
+    mocks: {
+      $vuetify: {
+        breakpoint: {}
+      }
+    },
+    stubs: [
+      'router-view'
     ]
+  })
+}
 
-    const wrapper = mount(PostList, {
-      localVue,
-      vuetify,
-      propsData: {
-        posts
-      },
-      mocks: {
-        $vuetify: {
-          breakpoint: {}
-        }
-      },
-      stubs: [
-        'router-link'
-      ]
-    })
+describe('PostList.vue', () => {
+  it('renders all posts', () => {
+    const wrapper = factory()
 
     expect(wrapper.findAllComponents(PostPreview).length).toEqual(3)
   })
 
   it('navigates to post', () => {
-    const post = {
-      _id: '123',
-      communityId: '1',
-      user: {
-        username: 'matt'
-      },
-      createdAt: Date.now(),
-      title: 'test title',
-      text: 'test text'
-    }
-    const posts = [
-      post, post, post
-    ]
-
-    const wrapper = mount(PostList, {
-      localVue,
-      vuetify,
-      router,
-      propsData: {
-        posts
-      },
-      mocks: {
-        $vuetify: {
-          breakpoint: {}
-        }
-      },
-      stubs: [
-        'router-view'
-      ]
-    })
+    const wrapper = factory()
 
     wrapper.find('a').trigger('click')
 
-    expect(router.history.pending.path).toEqual('/r/space/123')
+    expect(router.history.pending.path).toEqual(`/r/space/${posts[0]._id}`)
   })
 })
